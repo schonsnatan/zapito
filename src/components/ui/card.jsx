ed70d6fb41b25e95bcb205872f1327b468c3689a
schonsnatan <natanschons@gmail.com>
2025-05-22 23:56:14 -0300

    html simples

--- src/components/ui/card.jsx
+++
@@ -1,7 +0,0 @@
-
-export function Card({ children, className }) {
-  return <div className={`bg-white rounded-lg ${className}`}>{children}</div>;
-}
-export function CardContent({ children, className }) {
-  return <div>{children}</div>;
-}
